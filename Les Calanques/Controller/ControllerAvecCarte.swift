73f5c6d900e2f365b22d6fb09f07ab907981744b
Reggie <terareggie@hotmail.com>
2019-03-21 23:59:51 -0400

    Integration MapKitView & SegmentedControl & BarButtonItem

--- Les Calanques/Controller/ControllerAvecCarte.swift
+++ Les Calanques/Controller/ControllerAvecCarte.swift
@@ -7,24 +7,30 @@
 //
 
 import UIKit
+import MapKit
 
 class ControllerAvecCarte: UIViewController {
+    
+    
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    @IBAction func getPosition(_ sender: Any) {
     }
-    */
+    
+    
+    
+    @IBAction func segmentedChanged(_ sender: Any) {
+    }
+    
+    
+
 
 }
