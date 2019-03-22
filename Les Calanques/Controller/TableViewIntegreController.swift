4f02260445389a6e9bd68c072d9f84e4d9fdb16b
Reggie <terareggie@hotmail.com>
2019-03-21 22:38:15 -0400

    Logique Supprimer des Rows

--- Les Calanques/Controller/TableViewIntegreController.swift
+++ Les Calanques/Controller/TableViewIntegreController.swift
@@ -115,17 +115,36 @@ class TableViewIntegreController: UITableViewController
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            
+            calanques.remove(at: indexPath.row)
+            
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+            
+            print ("Je pourrais éventuellement ajouter un élément")
         }    
     }
-    */
+    
+    
+    
+    
+    @IBAction func reloadAction(_ sender: Any)
+    
+    {
+        
+        calanques = CalanqueCollection().all()
+        tableView.reloadData()
+    }
+    
+    
 
     /*
     // Override to support rearranging the table view.
