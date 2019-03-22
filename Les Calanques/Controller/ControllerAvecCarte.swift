f616113017d38dba1dd3f10e15650a65d26c12e3
Reggie <terareggie@hotmail.com>
2019-03-22 00:16:50 -0400

    Type Cartes & SegmentedControl (action)

--- Les Calanques/Controller/ControllerAvecCarte.swift
+++ Les Calanques/Controller/ControllerAvecCarte.swift
@@ -27,7 +27,16 @@ class ControllerAvecCarte: UIViewController {
     
     
     
-    @IBAction func segmentedChanged(_ sender: Any) {
+    @IBAction func segmentedChanged(_ sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex
+        
+        {
+        case 0: mapView.mapType = MKMapType.standard
+            case 1: mapView.mapType = .satellite
+            case 2: mapView.mapType = .hybrid
+            default: break
+        }
     }
     
     
